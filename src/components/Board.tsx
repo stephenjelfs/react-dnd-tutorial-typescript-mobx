0aabd276ae41c29d619911f5b752e712c9b05f08
Stephen Jelfs <stephen.jelfs@genedata.com>
2017-03-22 11:34:07 +0100

    Finished adding DnD support. Added custom react-dnd typings (to allow decorators).

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,65 +3,59 @@ import {observer} from "mobx-react";
 import {Game} from "../Game";
 import {BoardSquare} from "./BoardSquare";
 import {Knight} from "./Knight";
-import {DragDropContext, Backend, ContextComponentClass} from "react-dnd";
+import {DragDropContext, Backend} from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
 
 export interface BoardProps {
-  game: Game;
+    game: Game;
 }
-
-// for decorator...
-// declare function DragDropContext<P>(
-//     backend: Backend
-// ): <P, TFunction extends ContextComponentClass<P | void>>(componentClass: TFunction) => TFunction;
 
 @DragDropContext(HTML5Backend)
 @observer
 export class Board extends React.Component<BoardProps, {}> {
-  render() {
-    const squares = [];
-    for (let i = 0; i < 64; i++) {
-      squares.push(this.renderSquare(i));
+    render() {
+        const squares = [];
+        for (let i = 0; i < 64; i++) {
+            squares.push(this.renderSquare(i));
+        }
+
+        return (
+            <div style={{
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                flexWrap: 'wrap'}}>
+                {squares}
+            </div>
+        );
     }
 
-    return (
-      <div style={{
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        flexWrap: 'wrap'
-      }}>
-        {squares}
-      </div>
-    );
-  }
-
-  renderSquare(i: number) {
-    const x = i % 8;
-    const y = Math.floor(i / 8);
-    return (
-      <div key={i}
-           style={{ width: '12.5%', height: '12.5%' }}>
-        <BoardSquare x={x}
-                     y={y}>
-          {this.renderPiece(x, y)}
-        </BoardSquare>
-      </div>
-    );
-  }
-
-  renderPiece(x: number, y: number) {
-    const [knightX, knightY] = this.props.knightPosition;
-    if (x === knightX && y === knightY) {
-      return <Knight />;
+    renderSquare(i: number) {
+        const {game} = this.props;
+        const x = i % 8;
+        const y = Math.floor(i / 8);
+        return (
+            <div key={i}
+                 style={{ width: '12.5%', height: '12.5%' }}>
+                <BoardSquare game={game} x={x} y={y}>
+                    {this.renderPiece(x, y)}
+                </BoardSquare>
+            </div>
+        );
     }
-  }
 
-  private handleSquareClick(toX: number, toY: number) {
-    const {game} = this.props;
-
-    if (game.canMoveKnight(toX, toY)) {
-      game.moveKnight(toX, toY);
+    renderPiece(x: number, y: number) {
+        const [knightX, knightY] = this.props.game.knightPosition;
+        if (x === knightX && y === knightY) {
+            return <Knight />;
+        }
     }
-  }
+
+    private handleSquareClick(toX: number, toY: number) {
+        const {game} = this.props;
+
+        if (game.canMoveKnight(toX, toY)) {
+            game.moveKnight(toX, toY);
+        }
+    }
 }
--- src/components/BoardSquare.tsx
+++ src/components/BoardSquare.tsx
@@ -3,29 +3,67 @@ import {observer} from "mobx-react";
 import {Game} from "../Game";
 import {Square} from "./Square";
 import {Knight} from "./Knight";
-import {DragTarget, DragTargetSpec, ConnectDragTarger, DragTargetCollector} from "react-dnd";
+import {DropTarget, DropTargetSpec, DropTargetCollector, ConnectDropTarget} from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
+import {ItemTypes} from "../Constants";
 
-const squareTarget: DragTargetSpec = {
-  drop(props, monitor) {
-    moveKnight(props.x, props.y);
-  }
+const squareTarget: DropTargetSpec<BoardSquareProps> = {
+    canDrop(props) {
+        return props.game.canMoveKnight(props.x, props.y);
+    },
+      drop(props) {
+        props.game.moveKnight(props.x, props.y);
+      }
 };
 
-export interface BoardSquareProps {
-  x: number;
-  y: number;
+const collect: DropTargetCollector = (connect, monitor) => {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+      canDrop: monitor.canDrop()
+  };
 }
 
-export class BoardSquare extends React.Component<BoardSquareProps, undefined> {
-  render() {
-    const { x, y } = this.props;
-    const black = (x + y) % 2 === 1;
+export interface BoardSquareProps {
+    game: Game;
+    x: number;
+    y: number;
+    connectDropTarget?: ConnectDropTarget;
+    isOver?: boolean;
+    canDrop?: boolean;
+}
 
-    return (
-      <Square black={black}>
-        {this.props.children}
-      </Square>
-    );
-  }
+@DropTarget(ItemTypes.KNIGHT, squareTarget, collect)
+export class BoardSquare extends React.Component<BoardSquareProps, undefined> {
+
+    renderOverlay(color: string) {
+        return (
+            <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+        zIndex: 1,
+        opacity: 0.5,
+        backgroundColor: color,
+      }} />
+        );
+    }
+
+    render() {
+        const { x, y, connectDropTarget, isOver, canDrop } = this.props;
+        const black = (x + y) % 2 === 1;
+
+        return connectDropTarget(
+            <div style={{ position: 'relative', width: "100%", height: "100%" }}>
+                <Square black={black}>
+                    {this.props.children}
+                </Square>
+                {isOver && !canDrop && this.renderOverlay('red')}
+                {!isOver && canDrop && this.renderOverlay('yellow')}
+                {isOver && canDrop && this.renderOverlay('green')}
+            </div>
+        );
+    }
 }
